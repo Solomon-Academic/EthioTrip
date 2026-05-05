c8e43c2d2d802d2a3a0acdf2557c96cb5d77265c
Yordanos Abebe Getachew <Yordanos.abebe@aastustudent.edu.et>
2026-05-05 20:48:31 +0300

    Update create-destination.php
    add basic destination form

--- backend/create-destination.php
+++ backend/create-destination.php
@@ -2,3 +2,9 @@
 session_start();
 require_once 'config/database.php';
 ?>
+<form method="POST">
+    <input type="text" name="name" placeholder="Name"><br>
+    <input type="text" name="location" placeholder="Location"><br>
+    <textarea name="description" placeholder="Description"></textarea><br>
+    <button type="submit">Create</button>
+</form>
